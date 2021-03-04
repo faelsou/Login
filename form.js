451c59adaab7b1c310779d59565f8233b0f97bfe
Rafael <rafael@MBP-de-admin.lan>
2021-03-04 01:15:49 -0300

    first code

---
+++ form.js
@@ -0,0 +1,25 @@
+
+/*function funcao(){
+  const email = document.querySelector('#email').value;
+  const senha = document.querySelector('#password').value;
+  const botao = document.querySelector('#button');
+  console.log(email, senha)
+};*/
+
+function funcao(){
+  let email = document.getElementById('email').value;
+  let senha = document.getElementById('password').value;
+  let botao = document.getElementById("button");
+  //alert(email)
+ console.log(email, senha)
+};
+
+
+
+
+
+
+
+
+
+
